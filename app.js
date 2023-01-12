c5f326dc022a59b1189383493c12afc8da4084f9
Mario Huerta <mario.huerta@knights.ucf.edu>
2023-01-12 10:55:05 -0500

    Added active statuses depending on which section is getting clicked on

--- app.js
+++ app.js
@@ -23,6 +23,36 @@ function pageTransitions () {
             this.className += ' active-btn'
         })
     }
+
+    //Sections Active Class
+    //e is for event
+    allSections.addEventListener('click', (e) => {
+        //this targets the element that we click (visible in console)
+        //console.log(e.target);
+
+        //the id gets whichever dataset we click
+        const id = e.target.dataset.id;
+
+        //if we were able to grab an id
+        if (id){
+            //Remove selected button from the rest
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+
+            //We want to add the class to whatever button we clicked on
+            e.target.classList.add('active')
+
+            //Hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            //Element stores whatever id gets passed in
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 //Page Transitions call
